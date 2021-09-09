77960d9b0634ee6221c65dfed37f59e6519227bd
japhendywijaya <jap_hendy_wijaya@yahoo.co.id>
2021-09-09 18:19:32 +0700

    jap

--- config/config.js
+++ config/config.js
@@ -19,10 +19,10 @@ module.exports = {
     }
   },
   production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
+    username: process.env.PROD_DB_USERNAME || 'arpuyiolglmhza',
+    password: process.env.PROD_DB_PASSWORD || '',
     database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
+    host: process.env.PROD_DB_HOST,
     port: process.env.PROD_DB_PORT,
     dialect: 'postgres',
     dialectOptions: {
