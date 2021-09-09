c0ac69198bb89a5854b80874bc025005c0041b83
japhendywijaya <jap_hendy_wijaya@yahoo.co.id>
2021-09-09 18:13:59 +0700

    jap

--- config/config.js
+++ config/config.js
@@ -18,17 +18,6 @@ module.exports = {
     },
     }
   },
-  test: {
-    username: process.env.CI_DB_USERNAME,
-    password: process.env.CI_DB_PASSWORD,
-    database: process.env.CI_DB_NAME,
-    host: '127.0.0.1',
-    port: 3306,
-    dialect: 'postgres',
-    dialectOptions: {
-      bigNumberStrings: true
-    }
-  },
   production: {
     username: process.env.PROD_DB_USERNAME,
     password: process.env.PROD_DB_PASSWORD,
