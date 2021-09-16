980504f9318da059b19c68f41024c253034edd96
japhendywijaya <jap_hendy_wijaya@yahoo.co.id>
2021-09-16 17:21:12 +0700

    jap

--- controller/TestController.js
+++ controller/TestController.js
@@ -10,7 +10,7 @@ class TestController{
 
     static postRequest(req,res,next){
         postPayloads.create({
-            payload: req.body
+            payload: String(req.body)
         })
         .then(result=>{
             res.status(200).json({ message: 'PostRequest success' })
