3cbecf0c14b561821cb6cd9c4076d1e1c9d1cd58
japhendywijaya <jap_hendy_wijaya@yahoo.co.id>
2021-10-01 11:33:07 +0700

    update

--- route/index.js
+++ route/index.js
@@ -4,14 +4,12 @@ const Router = require('express').Router()
 
 Router.use('/tests', require('./testRoute'))
 Router.use('/', (req,res)=>{
-    res.status(200).json({
-        info: `
-            <POST> {{host}}/tests/postRequest => POST something
-            <GET> {{host}}/tests => List everything
-            <GET> {{host}}/tests/test => Test Connection
+    res.send(`
+    <POST> {{host}}/tests/postRequest => POST something
+    <GET> {{host}}/tests => List everything
+    <GET> {{host}}/tests/test => Test Connection
 
-        `
-    })
+    `)
 })
 
 
