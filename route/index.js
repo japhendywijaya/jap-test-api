8ee1324055d742af7abc3397bbe8bd26b5b3e614
japhendywijaya <jap_hendy_wijaya@yahoo.co.id>
2021-10-01 11:25:23 +0700

    add info;

--- route/index.js
+++ route/index.js
@@ -1,7 +1,18 @@
 const Router = require('express').Router()
 
 
+
 Router.use('/tests', require('./testRoute'))
-//comment
+Router.use('/', (req,res)=>{
+    res.status(200).json({
+        info: `
+            <POST> {{host}}/tests/postRequest => POST something
+            <GET> {{host}}/tests => List everything
+            <GET> {{host}}/tests/test => Test Connection
+
+        `
+    })
+})
+
 
 module.exports = Router
